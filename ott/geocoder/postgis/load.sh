f88de341da07108c1cb9af6737cd364d39d51239
Frank Purcell <fxpurcell@gmail.com>
2014-09-04 11:08:36 -0700

    second way to do geocoder

--- ott/geocoder/postgis/load.sh
+++ ott/geocoder/postgis/load.sh
@@ -1,15 +1,14 @@
-TMPDIR="/gisdata/temp/"
+TMPDIR="tmp"
 UNZIPTOOL=unzip
-WGETTOOL="/usr/bin/wget"
 export PGBIN=/opt/opengeo/pgsql/9.1/bin/
 export PGPORT=5432
 export PGHOST=localhost
+export PGDATABASE=trimet
 export PGUSER=postgres
-export PGPASSWORD=yourpasswordhere
-export PGDATABASE=geocoder
+export PGPASSWORD=
 PSQL=${PGBIN}/psql
 SHP2PGSQL=${PGBIN}/shp2pgsql
 
 wget http://www2.census.gov/geo/pvs/tiger2010st/41_Oregon/ --no-parent --relative --recursive --level=2 --accept=zip,txt --mirror --reject=html
-
 wget http://www2.census.gov/geo/pvs/tiger2010st/53_Washington/ --no-parent --relative --recursive --level=2 --accept=zip,txt --mirror --reject=html
+
