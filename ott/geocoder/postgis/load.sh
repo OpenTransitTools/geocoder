7ee161d16fc0c87e45bee7e6cd0eaf32a55dd580
Frank Purcell <fxpurcell@gmail.com>
2014-09-03 19:47:23 -0700

    uncomment wa

--- ott/geocoder/postgis/load.sh
+++ ott/geocoder/postgis/load.sh
@@ -12,4 +12,4 @@ SHP2PGSQL=${PGBIN}/shp2pgsql
 
 wget http://www2.census.gov/geo/pvs/tiger2010st/41_Oregon/ --no-parent --relative --recursive --level=2 --accept=zip,txt --mirror --reject=html
 
-#wget http://www2.census.gov/geo/pvs/tiger2010st/53_Washington/ --no-parent --relative --recursive --level=2 --accept=zip,txt --mirror --reject=html
+wget http://www2.census.gov/geo/pvs/tiger2010st/53_Washington/ --no-parent --relative --recursive --level=2 --accept=zip,txt --mirror --reject=html
